8babbd6e4c1fb8644e40133a7916195ebb9ae07b
KevinW2027 <kevin.wang2027@gbwl.org>
2026-03-10 19:09:12 +0000

    commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+"""from datetime import datetime
+"""
+app = Flask(__name__)
+
+# Create SQLite database instance
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+# Define model of a to-do list task
+
+class Task(db.Model):
+    # db.Column reps a col in the database
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    # add new task into database
+    if request.method =='POST':
+        task_content = request.form('content')
+        new_task = Task(content=task_content)
+        # Put new Task in Database
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error adding task!'
+    all_tasks = Task.query.all()
+    return render_template('index.html', task=all_tasks)
+
+
+
+
+
+
+
+
+
+
+
+
+# Create Database in Main method
+
+if __name__ == "main":
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
